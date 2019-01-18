e8608f82f91502948f54d04a8771e93353bc3ab9
Alexeis Vladimir Carrillo Pinaya <acarrillo@mail.rua.net>
2019-01-18 15:51:27 -0400

    adecuaciones 2

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var infoRoute = require('./routes/info-route');
 var modelRoute = require('./routes/model-route');
 var app = express();
 
-const PORT = config.port || 3000
-
+var port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -16,7 +15,7 @@ app.get("/",(req,res)=>{
 // http://localhost:3000/usuario/insert
 app.use("/info",infoRoute);
 app.use("/model",modelRoute);
-app.listen(PORT,()=>{
+app.listen(port,()=>{
     console.log("Servidor inicializado en puerto" + PORT)
 })
 
